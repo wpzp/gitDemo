5aceaed5d12a10a89f9faa08bd31836e36078c45
W <2580355377@qq.com>
2021-05-23 21:39:04 +0800

    hot_fix commit 1

--- src/main/java/com/git/demo/GitDemo.java
+++ src/main/java/com/git/demo/GitDemo.java
@@ -6,5 +6,6 @@ public class GitDemo {
         System.out.println("Hello Git1...");
         System.out.println("Hello Git2...");
         System.out.println("Hello Git3...");
+        System.out.println("Hello Git4...");
     }
 }
